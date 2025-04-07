86a3d5a2603efb4ae808e8cf3ec61d89933365bb
SadColourfulHues <sadcolourfulhues@gmail.com>
2025-04-08 00:19:57 +0800

    (misc) changed FlagNode behaviour to be more consistent with BehaviourTree's flagging utils

--- Utils/check_flag_node.gd
+++ Utils/check_flag_node.gd
@@ -1,5 +1,4 @@
 ## This conditional node returns SUCCESS if the specified flag exists in the context
-## Set [unflag_mode] to true to invert its behaviour.
 class_name CheckFlagNode
 extends BehaviourNode
 
@@ -7,14 +6,17 @@ var m_flag: StringName
 var m_check_target: bool
 
 
-func _init(flag: StringName, unflag_mode: bool = true) -> void:
+func _init(flag: StringName, check_target: bool = true) -> void:
     m_flag = flag
-    m_check_target = unflag_mode
+    m_check_target = check_target
 
 
 #region Events
 
 func _on_behaviour_process(context: Dictionary[StringName, Variant]) -> BehaviourNode.Result:
-    return BTUtils.b2r(context.has(m_flag) && context[m_flag] == m_check_target)
+    if context.has(m_flag) && context[m_flag] == m_check_target:
+        return BehaviourNode.Result.SUCCESS
+
+    return BehaviourNode.Result.FAIL
 
 #endregion
--- Utils/flag_node.gd
+++ Utils/flag_node.gd
@@ -1,8 +1,24 @@
-## A simple variant of the SetVarNode that uses an ID/boolean pair
-## Set [unflag_mode] in its constructor to perform an unflag.
+## Sets/unsets a behaviour flag in the processing context
+## (Try to limit where this is used. aka. treat it like a callback node
+## i.e. end of conditional chains to reduce write costs.)
 class_name FlagNode
-extends SetVarNode
+extends BehaviourNode
+
+var m_key: StringName
+var m_flag_mode: bool
 
 
-func _init(key: StringName, unflag_mode: bool = false) -> void:
-    super._init(key, !unflag_mode)
+func _init(key: StringName, flag_mode: bool) -> void:
+    m_key = key
+    m_flag_mode = flag_mode
+
+
+#region Events
+
+func _on_behaviour_process(context: Dictionary[StringName, Variant]) -> BehaviourNode.Result:
+    if m_flag_mode:
+        context[m_key] = true
+    else:
+        context.erase(m_key)
+
+    return BehaviourNode.Result.SUCCESS
